06a40de43691e87cfa85585501e96d0f02f7fd35
Julio <jcsmerubia@gmail.com>
2023-10-24 09:33:57 +0200

    Logo

---
+++ huliatlon/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import LogoImg from './components/Logo.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LogoImg />
+        <p>
+          
+        </p>
+        <a
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ huliatlon/src/components/Logo.js
@@ -0,0 +1,10 @@
+import Logo from '../images/decaclon.png'
+
+function LogoImg(){
+    return(
+        <img src={Logo} alt="Logo Decaclon" />
+    );
+}
+
+
+export default LogoImg;
